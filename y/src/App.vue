d23c790526ba51cfa11a031182242d3268721d1e
veniti <czarnecka.wer@gmail.com>
2025-10-14 11:06:46 +0200

    commit

---
+++ y/src/App.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+  <img alt="Kriżerowo logo" class="logo" src="./assets/cat.webp" width="205" height="125" />
+
+    <div class="wrapper">
+        <h2>Monika Kriże</h2>
+  <br>
+  <h3>księgowa twoich marzeń</h3>
+    </div>
+    
+  </header>
+
+  <main>
+
+    <TheWelcome />
+  </main>
+</template>
+
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
+
---
+++ y/src/components/TheWelcome.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+
+<h1> Monika Kriże <br> Twoja księgowa z ludzkim podejściem </h1>
+<br><br>
+Nazywam się Monika Kriże i od lat wspieram mikroprzedsiębiorców w codziennym prowadzeniu księgowości. Specjalizuję się w obsłudze małych firm, jednoosobowych działalności oraz branż kreatywnych i usługowych – takich jak kosmetologia, fryzjerstwo, e-commerce czy wolne zawody.
+
+<br><br>
+Działam pod marką Kriżerowo.pl, gdzie oferuję:
+<br>
+✔️ wystawianie i ewidencjonowanie faktur,<br>
+✔️ bieżące rozliczenia,<br>
+✔️ przygotowanie dokumentów do urzędów,<br>
+✔️ doradztwo w zakresie składek i ulg (np. ulga dla młodych),<br>
+✔️ miłą, spokojną i zrozumiałą współpracę bez niepotrzebnego żargonu.<br>
+<br><br>
+Zlokalizowana jestem w Gdańsku, ale współpracuję z klientami z całej Polski – zdalnie, sprawnie i bez stresu.<br><br>
+
+📞 Masz pytanie? Zadzwoń: 213 742 069<br>
+✉️ Albo napisz: kontakt@kriżerowo.pl<br>
+<br><br>
+Faktury bez zbędnej biurokracji, a księgowość z uśmiechem.<br>
+
+
+
+</template>
